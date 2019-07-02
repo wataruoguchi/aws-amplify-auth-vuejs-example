c834f2f6d1e6c0ef4c10b154b6abdbf50542ed99
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-07-01 18:18:57 -0700

    Minor change - Adding CSS to make it look nicer. Adding an ESlint rule

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,13 @@
     <router-view/>
   </div>
 </template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
