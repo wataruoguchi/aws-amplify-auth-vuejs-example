489fa68470964067129dff52c91d44acadff2f06
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-07-01 18:21:20 -0700

    Setting up Vue Router

--- src/router.js
+++ src/router.js
@@ -20,6 +20,21 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/signUp',
+      name: 'signUp',
+      component: () => import(/* webpackChunkName: "about" */ './views/SignUp.vue')
+    },
+    {
+      path: '/signUpConfirm',
+      name: 'signUpConfirm',
+      component: () => import(/* webpackChunkName: "about" */ './views/SignUpConfirm.vue')
+    },
+    {
+      path: '/signIn',
+      name: 'signIn',
+      component: () => import(/* webpackChunkName: "about" */ './views/SignIn.vue')
+    },
   ]
 })
