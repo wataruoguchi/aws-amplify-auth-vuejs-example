6cab86895cf8c6f99eef8ced6b75f90c8a6af9b7
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-07-01 19:32:34 -0700

    Update the Vue Router

--- src/router.js
+++ src/router.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import {getUser} from '@/utils/auth.js'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: { requiresAuth: true },
     },
     {
       path: '/about',
@@ -24,17 +27,69 @@ export default new Router({
     {
       path: '/signUp',
       name: 'signUp',
-      component: () => import(/* webpackChunkName: "about" */ './views/SignUp.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/SignUp.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/signUpConfirm',
       name: 'signUpConfirm',
-      component: () => import(/* webpackChunkName: "about" */ './views/SignUpConfirm.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/SignUpConfirm.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/signIn',
       name: 'signIn',
-      component: () => import(/* webpackChunkName: "about" */ './views/SignIn.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/SignIn.vue'),
+      meta: { requiresAuth: false },
     },
   ]
 })
+
+getUser().then((user) => {
+  if (user) {
+    router.push({path: '/'})
+  }
+})
+
+AmplifyEventBus.$on('authState', async (state) => {
+  const pushPathes = {
+    signedOut: () => {
+      router.push({path: '/signIn'})
+    },
+    signUp: () => {
+      router.push({path: '/signUp'})
+    },
+    confirmSignUp: () => {
+      router.push({path: '/signUpConfirm'})
+    },
+    signIn: () => {
+      router.push({path: '/signIn'})
+    },
+    signedIn: () => {
+      router.push({path: '/'})
+    }
+  }
+  if (typeof pushPathes[state] === 'function') {
+    pushPathes[state]()
+  }
+})
+
+router.beforeResolve(async (to, from, next) => {
+  const user = await getUser()
+  if (!user) {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+      return next({
+        path: '/signIn',
+      })
+    }
+  } else {
+    if (to.matched.some((record) => typeof(record.meta.requiresAuth) === "boolean" && !record.meta.requiresAuth)) {
+      return next({
+        path: '/',
+      })
+    }
+  }
+  return next()
+})
+
+export default router
