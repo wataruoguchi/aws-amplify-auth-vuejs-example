1244bf8dcaf56482fb378d244a854c8b4c4a9915
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-07-03 21:16:04 -0700

    Style tweaks
    - Removing HelloWorld - we don't use it.
    - Make the menu nicer with v-navigation-drawer and v-toolbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
+    <div>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
     <v-btn @click="signOut">Sign Out</v-btn>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import {signOut} from '@/utils/auth.js'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  },
   methods: {
     signOut() {
       signOut().then((data) => console.log('DONE', data)).catch((err) => console.log('SIGN OUT ERR', err));
